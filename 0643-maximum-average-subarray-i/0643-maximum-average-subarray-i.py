2800494304600245a4e8b8f35521a68492358f4b
Sahithyan04 <135291706+Sahithyan04@users.noreply.github.com>
2025-01-28 22:15:25 +0530

    0643-MAXIMUM-AVERAGE-SUBARRAY-I Stats: Time: 51 ms (91.82%), Space: 27.5 MB (8.29%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(k,len(nums)):
             sum_val += nums[i]
             sum_val -= nums[i-k]
-            max_avg = max(max_avg, sum_val/k)
+            max_avg = (max_avg if max_avg>(sum_val/k) else sum_val/k)
         return max_avg
